1f5218667b112f9f040fc446b203f82056ec2f67
harsolaAyu <aayush.harsola@ilink-systems.com>
2025-01-15 13:56:16 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,16 @@
+function skillsmember() {
+  return [
+    {
+      name: 'Rahul',
+      skills: ['JavaScript', 'React', 'Node']
+    },
+    {
+      name: 'Ritesh',
+      skills: ['Java', 'Spring', 'Hibernate']
+    },
+    {
+      name: 'Sachin',
+      skills: ['Python', 'Django', 'Flask']
+    }
+  ];
+}
